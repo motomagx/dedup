9f0e7538c8c225071640cc39a1819e9b386be178
motomagx <40811525+motomagx@users.noreply.github.com>
2024-12-19 20:08:23 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-TEMP_VAULT_DIR="/vault/temp"
-STORAGE_VAULT_DIR="/vault/storage"
-HASH_VAULT_DIR="/vault/hash"
-FILES_VAULT_DIR="/vault/files"
+MAIN_DIR="/vault"
+TEMP_VAULT_DIR="$MAIN_DIR/temp"
+STORAGE_VAULT_DIR="$MAIN_DIR/storage"
+HASH_VAULT_DIR="$MAIN_DIR/hash"
+FILES_VAULT_DIR="$MAIN_DIR/files"
+HASH_SNAPSHOTS="$MAIN_DIR/hash_snapshots"
+STORAGE_SNAPSHOTS="$MAIN_DIR/storage_snapshots"
 ARGS="$1"
 
 mkdir -p "$FILES_VAULT_DIR"
 mkdir -p "$HASH_VAULT_DIR"
 mkdir -p "$STORAGE_VAULT_DIR"
 mkdir -p "$TEMP_VAULT_DIR"
+mkdir -p "$STORAGE_SNAPSHOTS"
+mkdir -p "$HASH_SNAPSHOTS"
 
-#if [ -d "/ramdisk/hash" ]
-#then
-#	echo "Removendo ramdisk..."
-#	rm -r "/ramdisk/hash"
-#fi
-
-#echo "Carregando RAMDISK..."
-#mkdir -p "/ramdisk"
-#cp -r "/vault/hash" "/ramdisk/hash"
-mkdir -p /vault/hash.old
 echo "Analizando arquivos, aguarde..."
-
-if [ ! -f /vault/hash.old/counter ]
-then
-	echo "0" > /vault/hash.old/counter
-fi
 
 # Iterar sobre todos os arquivos no diretório e subdiretórios
 find "$TEMP_VAULT_DIR" -type f | while IFS= read -r file; do
@@ -44,7 +34,6 @@ find "$TEMP_VAULT_DIR" -type f | while IFS= read -r file; do
            mkdir -p "$FILE_DIR"
 	   mv "$file" "/vault/files/$hash"
 	   ln -s "/vault/files/$hash" "$NEW_FILE"
-	   #echo  "/vault/files/$hash $NEW_FILE"
 	   echo "$file" >> "/vault/hash/$hash/files.txt"
     	   echo "[$(date +%H:%M:%S)] ADD: $hash $file"
 	else
@@ -56,31 +45,23 @@ find "$TEMP_VAULT_DIR" -type f | while IFS= read -r file; do
 	        mkdir -p "$FILE_DIR"
 		ln -s "/vault/files/$hash" "$NEW_FILE"
 
-		#if [ "x$ARGS" == "xdebug" ]
-		#then
-			
-	     	        #echo "[$(date +%H:%M:%S)] UPD: $hash $file"
+		if [ "x$ARGS" == "xdebug" ]
+		then
 			SIZE=$(du "$NEW_FILE" -h -L)
                         echo "[$(date +%H:%M:%S)] UPD: $SIZE $hash $file"
-		#fi
+		fi
 	   fi
 	fi
-
 done
 
-COUNTER=$( cat /vault/hash.old/counter )
-COUNTER=$(($COUNTER+1))
-
-echo "$COUNTER" > /vault/hash.old/counter
-
-echo "Criando backup da tabela hash atual..."
-mksquashfs /vault/hash /vault/hash.old/hash_backup_$COUNTER.squashfs
-#rm -r /vault/hash
-
-#echo "Descarregando nova tabela hash da ramdisk..."
-#mv -f /ramdisk/hash /vault/hash
-
-#rsync -av --include='*/' --exclude='*' /vault/temp/ /vault/storage/
-
+echo "Sincronizando pastas, aguarde..."
+rsync -av --include='*/' --exclude='*' /vault/temp/ /vault/storage/
 find /vault/temp/ -type d -empty -delete
 mkdir -p /vault/temp 
+
+echo "Criando backup da tabela hash atual..."
+mksquashfs /vault/hash /vault/hash_snapshots/hash_$(date +%Y-%M-%d_%Hh%Mm%Ss).squashfs
+
+echo "Criando backup da estrutura compartilhada atual..."
+mksquashfs /vault/hash /vault/storage_snapshots/storage_$(date +%Y-%M-%d_%Hh%Mm%Ss).squashfs
+
